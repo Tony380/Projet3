c12c4d0eda903f7d335d84c84c04d75aa14fec61
Tony380 <tonygon2@yahoo.es>
2020-03-21 14:14:53 -0300

    Amélioration du jeu et commentaires

--- Classes.py
+++ Classes.py
@@ -1,16 +1,17 @@
+"""This file contains some variables and all the classes and their respective methods"""
+
 import pygame
 from random import randrange
 
+# lists of coordinates to build the maze
 coord_list_s = []
 coord_list_x = []
+
 wall = pygame.image.load("ressource/wall.png")
 floor = pygame.image.load("ressource/floor.png")
 
-play = pygame.image.load("ressource/play.jpg")
-win = pygame.image.load("ressource/win.jpg")
-lose = pygame.image.load("ressource/lose.jpg")
 
-
+# this class contains all other classes in order to charge them at once
 class Game:
 
     def __init__(self):
@@ -20,11 +21,16 @@ class Game:
         self.sting = Sting()
         self.syringe = Syringe()
         self.warden = Warden()
+        self.play = pygame.image.load("ressource/play.jpg").convert()
+        self.win = pygame.image.load("ressource/win.jpg").convert()
+        self.lose = pygame.image.load("ressource/lose.jpg").convert()
+
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.objects = 0
         self.items = "empty"
         self.image = pygame.image.load("ressource/MacGyver.png")
         self.rect = self.image.get_rect()
@@ -46,6 +52,7 @@ class Ether(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("ressource/ether.png")
+        # gives some random coordinates for placing in the maze
         self.pos = coord_list_s[randrange(0, len(coord_list_s))]
         self.rect = self.image.get_rect()
         self.rect.x = self.pos[0]
@@ -83,7 +90,6 @@ class Syringe(pygame.sprite.Sprite):
         self.rect.x = 520
         self.rect.y = 480
         self.show = False
-
 
 
 class Warden(pygame.sprite.Sprite):
--- Main.py
+++ Main.py
@@ -1,20 +1,24 @@
+""" This is the main file which will run the game"""
+
 import pygame
 from Structure import maze_structure
 from Classes import *
 
+
 pygame.init()
 
-resolution = (600, 600)
 pygame.display.set_caption("Mac Gyver's Maze")
-screen = pygame.display.set_mode(resolution)
-
-
+screen = pygame.display.set_mode((600, 600))
+song = pygame.mixer.Sound("ressource/MacGyverSong.ogg")
+song.set_volume(0.02)
+song.play(10, 0)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
 
+    # this function will build the maze
     def maze():
         coord_y = 0
         for element in maze_structure:
@@ -42,15 +46,16 @@ while True:
 
     game = Game()
 
+    # this will avoid double coordinates
     while game.tube.pos == game.ether.pos:
         game.tube.pos = coord_list_s[randrange(0, len(coord_list_s))]
     while game.sting.pos == game.ether.pos or game.sting.pos == game.tube.pos:
         game.sting.pos = coord_list_s[randrange(0, len(coord_list_s))]
 
-
-    screen.blit(play, (0, 0))
+    screen.blit(game.play, (0, 0))
     menu = ""
 
+    # second loop will contain our menu
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -63,6 +68,7 @@ while True:
                 elif event.key == pygame.K_m:
                     menu = "m"
                 elif event.key == pygame.K_UP and (
+                        # player won't get out of the frame
                         game.player.rect.x, game.player.rect.y - 40) not in coord_list_x and \
                         game.player.rect.y - 40 > -40:
                     game.player.move("up")
@@ -76,35 +82,42 @@ while True:
                         game.player.rect.x - 40, game.player.rect.y) not in coord_list_x:
                     game.player.move("left")
 
+        # first menu : game
         if menu == "p":
             maze()
+
             screen.blit(game.player.image, game.player.rect)
 
             if game.ether.show:
+                game.ether.image.set_colorkey((0, 0, 0))
                 screen.blit(game.ether.image, game.ether.pos)
 
             if game.tube.show:
+                game.tube.image.set_colorkey((0, 0, 0))
                 screen.blit(game.tube.image, game.tube.pos)
 
             if game.sting.show:
                 screen.blit(game.sting.image, game.sting.pos)
 
             if game.syringe.show:
+                game.syringe.image.set_colorkey((255, 255, 255))
                 screen.blit(game.syringe.image, game.syringe.rect)
 
             if game.warden.show:
                 screen.blit(game.warden.image, game.warden.rect)
 
-            if game.player.rect == game.ether.rect:
+            # detection of player picking items
+            if game.player.rect.colliderect(game.ether.rect):
                 game.ether.show = False
-            elif game.player.rect == game.tube.rect:
+            elif game.player.rect.colliderect(game.tube.rect):
                 game.tube.show = False
-            elif game.player.rect == game.sting.rect:
+            elif game.player.rect.colliderect(game.sting.rect):
                 game.sting.show = False
-            elif game.player.rect == game.syringe.rect:
+            elif game.player.rect.colliderect(game.syringe.rect):
                 if game.ether.show is False and game.tube.show is False and game.sting.show is False:
                     game.player.items = "full"
 
+            # syringe will appear
             if game.ether.show is False and game.tube.show is False and game.sting.show is False:
                 game.syringe.show = True
 
@@ -112,14 +125,16 @@ while True:
                     is False and game.player.items == "full":
                 game.syringe.show = False
 
-
-            if game.player.items == "full" and game.player.rect == game.warden.rect:
+            # end of game with 2 options, win or lose
+            if game.player.items == "full" and game.player.rect.colliderect(game.warden.rect):
                 game.warden.show = False
-                screen.blit(win, (0, 0))
-            elif game.player.items == "empty" and game.player.rect == game.warden.rect:
-                screen.blit(lose, (0, 0))
+                screen.blit(game.win, (0, 0))
+                menu = ""
+            elif game.player.items == "empty" and game.player.rect.colliderect(game.warden.rect):
+                screen.blit(game.lose, (0, 0))
+                menu = ""
 
-
+        # back to the menu
         elif menu == "m":
             break
         elif menu == "e":
