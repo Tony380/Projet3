ea4969f3aee95c8566df8ffc36e6adb261503b96
Tony380 <tonygon2@yahoo.es>
2020-03-22 18:43:44 -0300

    Ajout de commentaires

--- Classes.py
+++ Classes.py
@@ -1,17 +1,17 @@
-"""This file contains some variables and all the classes and their respective methods"""
+"""This file contains some variables and all the classes and their respective methods.
+We import pygame here for loading images and inherit from sprite class"""
 
 import pygame
 from random import randrange
 import time
 
-# lists of coordinates to build the maze
+# lists of coordinates to build the maze and then give son coordinates to items
 coord_list_s = []
 coord_list_x = []
 
 
 # this class contains all other classes in order to charge them at once
 class Game:
-
     def __init__(self):
         self.pressed = {}
         self.player = Player()
@@ -25,17 +25,20 @@ class Game:
         self.lose = pygame.image.load("ressource/lose.jpg").convert()
 
 
-
+# this class represents MacGyver in the game and inherits from Sprite class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        # here we create a dictionary to save the items MacGyver picks up
         self.objects = {}
         self.items = "empty"
         self.image = pygame.image.load("ressource/MacGyver.png")
+        # self.rect indicates 4 figures about our object : <abscissa, ordered, width and height>
         self.rect = self.image.get_rect()
         self.rect.x = 40
         self.rect.y = 0
 
+    # method for player to move in the maze
     def move(self, direction):
         time.sleep(0.1)
         if direction == "up":
@@ -48,6 +51,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 40
 
 
+# below classes which represents the items MacGyver has to pick up
 class Ether(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -55,6 +59,7 @@ class Ether(pygame.sprite.Sprite):
         # gives some random coordinates for placing in the maze
         self.pos = coord_list_s[randrange(0, len(coord_list_s))]
         self.rect = self.image.get_rect()
+        # below we stock some coordinates on x and y for abscissa and ordered
         self.rect.x = self.pos[0]
         self.rect.y = self.pos[1]
         self.show = True
