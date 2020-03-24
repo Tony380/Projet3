d1d5903571751505ea30f5e44ab98a8d40223cdf
Tony380 <tonygon2@yahoo.es>
2020-03-24 13:09:24 -0300

    Actualisation du jeu + nouveaux fichiers

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ def main():
     guard = Items("guard", pygame.image.load("ressource/guard.png"))
     guard.place(screen, maze.coord_list_a)
 
-    ether = Items("ether", pygame.image.load("ressource/ether.png"))
-    pipe = Items("pipe", pygame.image.load("ressource/pipe.png"))
+    ether = Items("ether", pygame.image.load("ressource/ether.png").convert())
+    ether.image.set_colorkey((0, 0, 0))
+    pipe = Items("pipe", pygame.image.load("ressource/pipe.png").convert())
+    pipe.image.set_colorkey((0, 0, 0))
     needle = Items("needle", pygame.image.load("ressource/needle.png"))
 
     ether.place(screen, maze.coord_list_o)
@@ -29,45 +31,50 @@ def main():
 
     run = True
     while run:
-        pygame.time.Clock().tick(10)
-        screen.blit(maze.floor, player.rect)
+        if player.rect != guard.rect:
+            pygame.time.Clock().tick(10)
+            screen.blit(maze.floor, player.rect)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                elif event.type == pygame.KEYDOWN:
+                    player.pressed[event.key] = True
+                elif event.type == pygame.KEYUP:
+                    player.pressed[event.key] = False
+
+            if player.pressed.get(pygame.K_UP) and (player.rect.x, player.rect.y - 40) \
+                    not in maze.coord_list_x and player.rect.y - 40 >= 0:
+                player.move("up")
+            elif player.pressed.get(pygame.K_DOWN) and (player.rect.x, player.rect.y + 40) \
+                    not in maze.coord_list_x and player.rect.y + 40 < 600:
+                player.move("down")
+            elif player.pressed.get(pygame.K_RIGHT) and (player.rect.x + 40, player.rect.y) \
+                    not in maze.coord_list_x:
+                player.move("right")
+            elif player.pressed.get(pygame.K_LEFT) and (player.rect.x - 40, player.rect.y) \
+                    not in maze.coord_list_x:
+                player.move("left")
+
+            if player.rect.colliderect(ether.rect):
+                player.objects["ether"] = ether
+            elif player.rect.colliderect(pipe.rect):
+                player.objects["pipe"] = ether
+            elif player.rect.colliderect(needle.rect):
+                player.objects["needle"] = ether
+
+            screen.blit(player.image, player.rect)
+
+            if player.rect.colliderect(guard.rect) and len(player.objects) == 3:
+                win = pygame.image.load("ressource/win.jpg")
+                screen.blit(win, (0, 0))
                 run = False
-            elif event.type == pygame.KEYDOWN:
-                player.pressed[event.key] = True
-            elif event.type == pygame.KEYUP:
-                player.pressed[event.key] = False
+            elif player.rect.colliderect(guard.rect) and len(player.objects) != 3:
+                lose = pygame.image.load("ressource/lose.jpg")
+                screen.blit(lose, (0, 0))
+                run = False
 
-        if player.pressed.get(pygame.K_UP) and (player.rect.x, player.rect.y - 40) \
-                not in maze.coord_list_x and player.rect.y - 40 >= 0:
-            player.move("up")
-        elif player.pressed.get(pygame.K_DOWN) and (player.rect.x, player.rect.y + 40) \
-                not in maze.coord_list_x and player.rect.y + 40 < 600:
-            player.move("down")
-        elif player.pressed.get(pygame.K_RIGHT) and (player.rect.x + 40, player.rect.y) \
-                not in maze.coord_list_x:
-            player.move("right")
-        elif player.pressed.get(pygame.K_LEFT) and (player.rect.x - 40, player.rect.y) \
-                not in maze.coord_list_x:
-            player.move("left")
-
-        if player.rect.colliderect(ether.rect):
-            player.objects["ether"] = ether
-        elif player.rect.colliderect(pipe.rect):
-            player.objects["pipe"] = ether
-        elif player.rect.colliderect(needle.rect):
-            player.objects["needle"] = ether
-
-        if player.rect.colliderect(guard.rect) and len(player.objects) == 3:
-            print("win")
-        elif player.rect.colliderect(guard.rect) and len(player.objects) != 3:
-            print("lose")
-
-        screen.blit(player.image, player.rect)
-
-        pygame.display.flip()
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
